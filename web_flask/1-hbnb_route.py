ba3c23ffd4c58a5afee9796b2718abf87db32f6c
GKiarie <igitaukiarie@gmail.com>
2023-04-22 00:15:39 +0300

    update fle to executable

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,7 +14,7 @@ def hello():
 
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
-    """Ftn that returns Hello HBNB"""
+    """Ftn that returns HBNB"""
     return "HBNB"
 
 if __name__ == "__main__":
